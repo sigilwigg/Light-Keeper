418b147a3ea9f651a0828b2b67d07d5de8a26dac
WyattHWilliams <66445057+WyattHWilliams@users.noreply.github.com>
2022-03-03 17:49:55 -0800

    feat(game): added dom element selection

---
+++ js/light-keeper/game.js
@@ -0,0 +1,7 @@
+// ========== [///// DOM SELECTION /////] ==========
+const camera = document.querySelector(".camera");
+const playerElement = document.querySelector(".player");
+const map = document.querySelector(".map");
+const pixelSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--pixel-size'));
+
+
