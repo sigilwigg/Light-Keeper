a66e3655cf108b6f84019d09b89ddab479ab7713
WyattHWilliams <66445057+WyattHWilliams@users.noreply.github.com>
2022-03-03 17:49:09 -0800

    feat(player.class): created player class refactored from tutorial
    original tutorial: https://www.youtube.com/watch?v=H3Fn33lYuE0&ab_channel=DrewConley
    
    refactored code to be class-based; i also separated out concerns like the movement and draw phase.
    
    will need to put draw map logic elsewhere, but it's good for now.

---
+++ js/light-keeper/classes/player.class.js
@@ -0,0 +1,42 @@
+class Player {
+    constructor(_element, _x, _y, _width, _height, _collision_box) {
+        this.element = _element;
+        this.x = _x;
+        this.y = _y;
+        this.width = _width;
+        this.height = _height;
+        this.speed = 1;
+    }
+
+    drawSelf() {
+        map.style.transform = `translate3d( ${-this.x * game.pixelSize + game.cameraOffsetLeft}px, ${-this.y * game.pixelSize + game.cameraOffsetTop}px, 0 )`;
+
+        this.element.style.transform = `translate3d( ${this.x * game.pixelSize}px, ${this.y * game.pixelSize}px, 0 )`;
+    }
+
+    handleMovement() {
+        const held_direction = Inputs.held_directions[0];
+
+        if (held_direction) {
+            switch (held_direction) {
+                case DIRECTIONS.right:
+                    this.x += this.speed;
+                    break;
+                case DIRECTIONS.left:
+                    this.x -= this.speed;
+                    break;
+                case DIRECTIONS.down:
+                    this.y += this.speed;
+                    break;
+                case DIRECTIONS.up:
+                    this.y -= this.speed;
+                    break;
+            }
+            this.element.setAttribute("facing", held_direction);
+        }
+
+        this.element.setAttribute("walking", held_direction ? "true" : "false");
+
+        this.drawSelf();
+    }
+}
