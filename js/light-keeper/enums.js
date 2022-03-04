c2b992cfeb6848439df5a3d2e18ab0a553d39eca
WyattHWilliams <66445057+WyattHWilliams@users.noreply.github.com>
2022-03-03 17:42:39 -0800

    feat(enums): add directions enum and keybind enum

---
+++ js/light-keeper/enums.js
@@ -0,0 +1,14 @@
+// ========== [///// ENUMS /////] ==========
+const DIRECTIONS = {
+    up: "up",
+    down: "down",
+    left: "left",
+    right: "right",
+}
+
+const KEYS = {
+    38: DIRECTIONS.up,
+    37: DIRECTIONS.left,
+    39: DIRECTIONS.right,
+    40: DIRECTIONS.down,
+}
