88e6a337a652b14b0cfcbae39c9cce94355213a5
WyattHWilliams <66445057+WyattHWilliams@users.noreply.github.com>
2022-03-05 14:10:20 -0800

    feat(testmap): create testmap data

---
+++ js/light-keeper/tilemaps/testmap.js
@@ -0,0 +1,17 @@
+const testMapData = {
+    widthInTiles: 3,
+    heightInTiles: 3,
+    tileData: [
+        { x: 0, y: 0, layer: "floor", type: "sidewalk" },
+        { x: 0, y: 1, layer: "floor", type: "sidewalk" },
+        { x: 0, y: 2, layer: "floor", type: "sidewalk" },
+
+        { x: 1, y: 0, layer: "floor", type: "sidewalk" },
+        { x: 1, y: 1, layer: "floor", type: "grass" },
+        { x: 1, y: 2, layer: "floor", type: "sidewalk" },
+
+        { x: 2, y: 0, layer: "floor", type: "sidewalk" },
+        { x: 2, y: 1, layer: "floor", type: "sidewalk" },
+        { x: 2, y: 2, layer: "floor", type: "sidewalk" },
+    ]
+}
