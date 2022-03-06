cf5827ac7b2341fc6daab8a4f3cf544f3987957e
WyattHWilliams <66445057+WyattHWilliams@users.noreply.github.com>
2022-03-05 17:02:13 -0800

    feat(testmap): add variation prop

--- js/light-keeper/tilemaps/testmap.js
+++ js/light-keeper/tilemaps/testmap.js
@@ -2,16 +2,16 @@ const testMapData = {
     widthInTiles: 3,
     heightInTiles: 3,
     tileData: [
-        { x: 0, y: 0, layer: "floor", type: "sidewalk" },
-        { x: 0, y: 1, layer: "floor", type: "sidewalk" },
-        { x: 0, y: 2, layer: "floor", type: "sidewalk" },
+        { x: 0, y: 0, layer: "floor", type: "sidewalk", variation: true },
+        { x: 0, y: 1, layer: "floor", type: "sidewalk", variation: true },
+        { x: 0, y: 2, layer: "floor", type: "sidewalk", variation: true },
 
-        { x: 1, y: 0, layer: "floor", type: "sidewalk" },
-        { x: 1, y: 1, layer: "floor", type: "grass" },
-        { x: 1, y: 2, layer: "floor", type: "sidewalk" },
+        { x: 1, y: 0, layer: "floor", type: "sidewalk", variation: true },
+        { x: 1, y: 1, layer: "floor", type: "grass", variation: false },
+        { x: 1, y: 2, layer: "floor", type: "sidewalk", variation: true },
 
-        { x: 2, y: 0, layer: "floor", type: "sidewalk" },
-        { x: 2, y: 1, layer: "floor", type: "sidewalk" },
-        { x: 2, y: 2, layer: "floor", type: "sidewalk" },
+        { x: 2, y: 0, layer: "floor", type: "sidewalk", variation: true },
+        { x: 2, y: 1, layer: "floor", type: "sidewalk", variation: true },
+        { x: 2, y: 2, layer: "floor", type: "sidewalk", variation: true },
     ]
 }
