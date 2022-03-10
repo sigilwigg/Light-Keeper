6c734886fe77b3e6aa0fc909e30383b7094a97a4
WyattHWilliams <66445057+WyattHWilliams@users.noreply.github.com>
2022-03-10 12:55:24 -0800

    feat(map.class): add method for floor light tiles

--- js/light-keeper/classes/map.class.js
+++ js/light-keeper/classes/map.class.js
@@ -4,6 +4,7 @@ class Map {
         this.tiles = {
             floor: {},
             floorDeco: {},
+            floorLighting: {},
             obj: {},
             objDeco: {},
         };
@@ -41,6 +42,10 @@ class Map {
             newTileset.classList.add(`v-${vNum}`);
         }
 
+        if (tile.attr) {
+            newTileset.setAttribute(`${tile.attr.name}`, tile.attr.val);
+        }
+
         // ----- finish and add to map -----
         newTile.append(newTileset);
         mapLayer.append(newTile);
@@ -52,11 +57,31 @@ class Map {
         tileData.forEach(tile => {
             let tileElement = this.makeTile(tile);
 
+            // ----- set the map tile data -----
             this.tiles[`${tile.layer}`][`${tile.x},${tile.y}`] = {
                 x: tile.x,
                 y: tile.y,
                 type: tile.type,
                 element: tileElement,
+            }
+
+            // ----- add floor lighting above floor tiles -----
+            if (tile.layer == "floor") {
+                let lightElement = this.makeTile({
+                    x: tile.x,
+                    y: tile.y,
+                    layer: "floorLighting",
+                    type: "lighting",
+                    attr: { name: "lightLevel", val: 0 }
+                })
+
+                // ----- set the lighting tile in map tile data -----
+                this.tiles["floorLighting"][`${tile.x},${tile.y}`] = {
+                    x: tile.x,
+                    y: tile.y,
+                    type: "lighting",
+                    element: lightElement,
+                }
             }
         });
     }
@@ -68,5 +93,48 @@ class Map {
 
         // ----- populate the map with the tiles -----
         this.generateTiles(mapData.tileData);
+    }
+
+    setLightLevel(tileKey, lightLevel) {
+        if (!map.tiles.floorLighting[tileKey]) return;
+        map.tiles.floorLighting[tileKey].element.firstElementChild.setAttribute("lightLevel", `${lightLevel}`);
+    }
+
+    updateLighting() {
+        let [pTileX, pTileY] = player.getTilePosition();
+        let light3 = [[pTileX, pTileY]]
+        let light2 = [[pTileX - 1, pTileY + 1], [pTileX, pTileY + 1], [pTileX + 1, pTileY + 1], [pTileX - 1, pTileY], [pTileX + 1, pTileY],
+        [pTileX - 1, pTileY - 1], [pTileX, pTileY - 1], [pTileX + 1, pTileY - 1],]
+
+        let light1 = [
+            [pTileX - 2, pTileY + 2], [pTileX - 1, pTileY + 2], [pTileX, pTileY + 2], [pTileX + 1, pTileY + 2], [pTileX + 2, pTileY + 2],
+            [pTileX - 2, pTileY + 1], [pTileX + 2, pTileY + 1],
+            [pTileX - 2, pTileY], [pTileX + 2, pTileY],
+            [pTileX - 2, pTileY - 1], [pTileX + 2, pTileY - 1],
+            [pTileX - 2, pTileY - 2], [pTileX - 1, pTileY - 2], [pTileX, pTileY - 2], [pTileX + 1, pTileY - 2], [pTileX + 2, pTileY - 2],
+        ]
+
+        let light0 = [
+            [pTileX - 3, pTileY + 3], [pTileX - 2, pTileY + 3], [pTileX - 1, pTileY + 3], [pTileX, pTileY + 3], [pTileX + 1, pTileY + 3], [pTileX + 2, pTileY + 3], [pTileX + 3, pTileY + 3],
+            [pTileX - 3, pTileY + 2], [pTileX + 3, pTileY + 2],
+            [pTileX - 3, pTileY + 1], [pTileX + 3, pTileY + 1],
+            [pTileX - 3, pTileY], [pTileX + 3, pTileY],
+            [pTileX - 3, pTileY - 1], [pTileX + 3, pTileY - 1],
+            [pTileX - 3, pTileY - 2], [pTileX + 3, pTileY - 2],
+            [pTileX - 3, pTileY - 3], [pTileX - 2, pTileY - 3], [pTileX - 1, pTileY - 3], [pTileX, pTileY - 3], [pTileX + 1, pTileY - 3], [pTileX + 2, pTileY - 3], [pTileX + 3, pTileY - 3],
+        ]
+
+        light3.forEach(point => {
+            this.setLightLevel(`${point[0]},${point[1]}`, 3)
+        });
+        light2.forEach(point => {
+            this.setLightLevel(`${point[0]},${point[1]}`, 2)
+        });
+        light1.forEach(point => {
+            this.setLightLevel(`${point[0]},${point[1]}`, 1)
+        });
+        light0.forEach(point => {
+            this.setLightLevel(`${point[0]},${point[1]}`, 0)
+        });
     }
 }
