c9345d8870bb9c94a21ed3c3c0a6a3669816fc6c
WyattHWilliams <66445057+WyattHWilliams@users.noreply.github.com>
2022-03-11 10:41:19 -0800

    feat(map.class): add directional lighting

--- js/light-keeper/classes/map.class.js
+++ js/light-keeper/classes/map.class.js
@@ -23,12 +23,12 @@ class Map {
     drawSelf() {
         this.element.style.transform = `translate3d( ${-player.x * renderer.pixelSize + renderer.cameraOffsetLeft}px, ${-player.y * renderer.pixelSize + renderer.cameraOffsetTop}px, 0 )`;
 
-        let lightingOffsetLeft = player.x * renderer.pixelSize - renderer.cameraOffsetLeft - (player.width / 2.75);
-        let lightingOffsetRight = player.y * renderer.pixelSize - renderer.cameraOffsetTop - (player.height)
+        let lightingOffsetLeft = player.x * renderer.pixelSize - renderer.cameraOffsetLeft;
+        let lightingOffsetTop = player.y * renderer.pixelSize - renderer.cameraOffsetTop
 
-        this.lighting.floor.style.transform = `translate3d( ${lightingOffsetLeft}px, ${lightingOffsetRight}px, 0 )`;
+        this.lighting.floor.style.transform = `translate3d( ${lightingOffsetLeft}px, ${lightingOffsetTop}px, 0 )`;
 
-        this.lighting.obj.style.transform = `translate3d( ${lightingOffsetLeft}px, ${lightingOffsetRight}px, 0 )`;
+        this.lighting.obj.style.transform = `translate3d( ${lightingOffsetLeft}px, ${lightingOffsetTop}px, 0 )`;
     }
 
     makeTile(tile) {
@@ -84,5 +84,110 @@ class Map {
 
         // ----- populate the map with the tiles -----
         this.generateTiles(mapData.tileData);
+    }
+
+    setObjLightDir(coords, val) {
+        this.tiles["obj"][coords].element.firstChild.setAttribute("light-dir", val);
+    }
+
+    updateLighting() {
+        let [pTileX, pTileY] = player.getTilePosition();
+        let lightDistance = 4;
+        let TL, TR, BL, BR;
+        let tileDistance = 1;
+        let coords = ""
+
+        function checkCurDist(i, ld, d) {
+            // if (i <= 1) return 0;
+            if (i >= ld + 1) return 1;
+            return d;
+        }
+
+        for (let i = 1; i <= lightDistance + 1; i++) {
+            TL = [pTileX - i, pTileY - i];
+            TR = [pTileX + i, pTileY - i];
+            BL = [pTileX - i, pTileY + i];
+            BR = [pTileX + i, pTileY + i];
+
+            tileDistance = (pTileX + i) - (pTileX - i);
+
+            for (let t = 1; t < tileDistance; t++) {
+                // ----- TL -----
+                coords = `${TL[0]},${TL[1]}`;
+                if (this.tiles.obj[coords]) {
+                    this.setObjLightDir(
+                        coords,
+                        checkCurDist(i, lightDistance, "TL")
+                    )
+                }
+
+                // ----- TR -----
+                coords = `${TR[0]},${TR[1]}`;
+                if (this.tiles.obj[coords]) {
+                    this.setObjLightDir(
+                        coords,
+                        checkCurDist(i, lightDistance, "TR")
+                    )
+                }
+
+                // ----- BL -----
+                coords = `${BL[0]},${BL[1]}`;
+                if (this.tiles.obj[coords]) {
+                    this.setObjLightDir(
+                        coords,
+                        checkCurDist(i, lightDistance, "BL")
+                    )
+                }
+
+                // ----- BR -----
+                coords = `${BR[0]},${BR[1]}`
+                if (this.tiles.obj[coords]) {
+                    this.setObjLightDir(
+                        coords,
+                        checkCurDist(i, lightDistance, "BR")
+                    )
+                }
+
+                // ----- below player -----
+                coords = `${BL[0] + t},${BL[1]}`;
+                if (this.tiles.obj[coords]) {
+
+                    this.setObjLightDir(
+                        coords,
+                        checkCurDist(i, lightDistance, "B")
+                    )
+                }
+
+                // ----- above player -----
+                coords = `${TL[0] + t},${TL[1]}`;
+                if (this.tiles.obj[coords]) {
+
+                    this.setObjLightDir(
+                        coords,
+                        checkCurDist(i, lightDistance, "T")
+                    )
+                }
+
+                // ----- left of player -----
+                coords = `${TL[0]},${BL[1] - t}`;
+                if (this.tiles.obj[coords]) {
+
+                    this.setObjLightDir(
+                        coords,
+                        checkCurDist(i, lightDistance, "L")
+                    )
+                }
+
+                // ----- right of player -----
+                coords = `${TR[0]},${BR[1] - t}`;
+                if (this.tiles.obj[coords]) {
+
+                    this.setObjLightDir(
+                        coords,
+                        checkCurDist(i, lightDistance, "R")
+                    )
+                }
+            }
+        }
     }
 }
